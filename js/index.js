af6ea29eadbb2b7c15ec22d46527757c9f0d0550
Josh Akeman <joshakeman@gmail.com>
2019-03-12 14:13:38 -0400

    finished mobile menu

--- js/index.js
+++ js/index.js
@@ -1,27 +1,44 @@
-$(function(){  // $(document).ready shorthand
-    $('.vertical-info-bar').fadeIn('slow');
-  });
-  
-  $(document).ready(function() {
-      
-      /* Every time the window is scrolled ... */
-      $(window).scroll( function(){
-      
-          /* Check the location of each desired element */
-          $('.hideme').each( function(i){
-              
-              var bottom_of_object = $(this).position().top + $(this).outerHeight();
-              var bottom_of_window = $(window).scrollTop() + $(window).height();
-              
-              /* If the object is completely visible in the window, fade it it */
-              if( bottom_of_window > bottom_of_object ){
-                  
-                  $(this).animate({'opacity':'1'},1500);
-                      
-              }
-              
-          }); 
-      
-      });
-      
-  });
+// $(document).ready(function() {
+//     var timeToDisplay = 2000;
+
+//     var slideshow = $('#div1');
+//     var urls = [
+//        '../img/heatmap1.jpg',
+//        '../img/Heatmap-sat50.jpg',
+//     ];
+
+//     var index = 0;
+//     var transition = function() {
+//         var url = urls[index];
+
+//         slideshow.css('background-image', 'url(' + url + ')');
+
+//         index = index + 1;
+//         if (index > urls.length - 1) {
+//             index = 0;
+//         }
+//     };
+
+//     var run = function() {
+//         transition();
+//         slideshow.fadeIn('slow', function() {
+//             setTimeout(function() {
+//                 slideshow.fadeOut('slow', run);
+//             }, timeToDisplay);
+//         });
+//     }
+
+//     run();
+// })​;
+
+// let pulse = document.querySelector('#div1');
+
+// window.addEventListener( "load", function(event) {
+//     console.log(pulse)
+// })
+
+const mobileDropNav = document.querySelector('.drop-menu');
+const mobileMenuButton = document.querySelector('.mobile-menu-icon');
+mobileMenuButton.addEventListener('click', function(){
+  mobileDropNav.classList.toggle('unhide');
+});
