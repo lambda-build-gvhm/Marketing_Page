d4415b7a5482c5df3e99d7c755bae2c8e3900772
Josh Akeman <joshakeman@gmail.com>
2019-03-12 20:46:29 -0400

    started on modal section

--- js/index.js
+++ js/index.js
@@ -5,7 +5,7 @@ let thing = document.querySelector('#div1');
 window.addEventListener( "load", function(event) {
     let num = 0;
 const interval = setInterval(()=> {
-    if(num===2) {
+    if(num===5) {
         clearInterval(interval);
         thing.style.backgroundImage = "url('img/heatmap1.jpg')";
     } else if (num % 2 ===0){
